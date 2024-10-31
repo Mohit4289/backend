460060ccb8a80991f7a0c8da17a020f8f86a48a5
Mohit4289 <168074292+Mohit4289@users.noreply.github.com>
2024-10-31 15:20:31 +0530

    db fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import cors from 'cors'
+import express from 'express'
+import CookieParser from 'cookie-parser'
 import {app} from './app.js'
+
 dotenv.config({
     path: './.env'
 })
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
 
 connectDB()
 .then(() => {
